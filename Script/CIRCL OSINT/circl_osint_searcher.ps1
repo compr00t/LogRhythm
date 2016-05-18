88222d075780d6f721ffd8f0b2ca26653a42e9df
Patrick <patrick.p.schmid@gmail.com>
2016-05-18 12:51:05 +0200

    Added workaround for large JSON files. And a script for searching the content

--- Script/CIRCL OSINT/circl_osint_searcher.ps1
+++ Script/CIRCL OSINT/circl_osint_searcher.ps1
@@ -1,5 +1,6 @@
-# Get your LR installation
-$lr = "YOUR_LR_SERVER_HERE"
+# Search parameter
+$search = "172.29.21.174"
+$ErrorActionPreference = 'Continue'
 
 # Settings
 function ConvertFrom-Json2{
@@ -17,8 +18,7 @@ param
         ParameterSetName='object',
         ValueFromPipeline=$true,
         Mandatory=$false)]
-        [int]
-        $MaxJsonLength = 67108864
+        [int]$MaxJsonLength = 67108864
 )
 
 BEGIN 
@@ -46,17 +46,14 @@ PROCESS
 END
 {
 }
+
 }
 
 # Get list of new intel
 Invoke-WebRequest -Uri https://www.circl.lu/doc/misp/feed-osint/ | Foreach { $_.Links.innerHTML } | Select-String "json" | Select-String "manifest.json" -NotMatch | Foreach-Object {Add-Content C:\Temp\intels.txt "https://www.circl.lu/doc/misp/feed-osint/$_"}
 
 # Download OSINT
-Get-Content C:\Temp\intels.txt | foreach { Invoke-WebRequest -Uri $_ | ConvertFrom-Json2 | foreach { $_.Event.Attribute } | Where-Object {($_.type -eq "ip-dst") -or ($_.type -eq "domain") -or ($_.type -eq "ip-src")} | foreach { $_.value } | Out-File C:\Temp\circl_temp.txt -Append }
+Get-Content C:\Temp\intels.txt | foreach { Invoke-WebRequest -Uri $_ | ConvertFrom-Json2 -ErrorAction SilentlyContinue -WarningAction SilentlyContinue | foreach { $_.Event.Attribute } | Where-Object { $_.value -eq $search } }
 
-# Copy to LogRhythm
-Copy-Item -Path C:\Temp\circl_temp.txt -Destination "\\$lr\C$\Program Files\LogRhythm\LogRhythm Job Manager\config\list_import\circl.txt"
-
-# Clean up
-# Remove-Item C:\Temp\intels.txt
-# Remove-Item C:\Temp\circl_temp.txt
+# Cleanup
+Remove-Item C:\Temp\intels.txt
